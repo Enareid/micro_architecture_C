79c5a8dd9dcaff9916b813f8893819ddfbf3e1de
Stephane Boudier <stephane.boudier.etu@a11p6.fil.univ-lille.fr>
2025-03-06 16:27:18 +0100

    mouai

--- TP1/decode.c
+++ TP1/decode.c
@@ -46,9 +46,9 @@ decode_instruction(int instruction, int taille)
 void 
 load_instructions(const char *filename) {
     FILE * file = fopen(filename, "r");
-    int file2 = open("test.txt", O_WRONLY | O_CREAT | O_TRUNC);
     if (!file) {
         perror("Erreur ouverture fichier");
+        fclose(file);
         exit(EXIT_FAILURE);
     }
     int indice = 0;
@@ -64,8 +64,6 @@ load_instructions(const char *filename) {
                 }
             }
         }
-        char * instruction = decode_instruction(values[0], 7).inst_ASM;
-        write(file2, instruction);
         if (indice == 0) {
             CPU.RI = values[0];
             CPU.adresse_latch = address;
@@ -79,92 +77,130 @@ void
 execute()
 {
     printf("Execute\n");
-    instruction_t instruction = decode_instruction(CPU.RI, 7);
+    int instruction = decode_instruction(CPU.RI, 7).code_op;
+    fflush(stdout);
     while(CPU.RI != 0) {
-        switch(instruction.code_op) {
+        switch(instruction) {
             case 0b111:
                 SWP((CPU.RI >> 3) && 0x07, CPU.RI & 0x07);
                 next_instru();
-                instruction = decode_instruction(CPU.RI, 7);
+                instruction = decode_instruction(CPU.RI, 7).code_op;
                 break;
             case 0b110:
                 AND((CPU.RI >> 3) && 0x07, CPU.RI & 0x07);
                 next_instru();
-                instruction = decode_instruction(CPU.RI, 7);
+                instruction = decode_instruction(CPU.RI, 7).code_op;
                 break;
             case 0b101:
                 SUB((CPU.RI >> 3) && 0x07, CPU.RI & 0x07);
                 next_instru();
-                instruction = decode_instruction(CPU.RI, 7);
+                instruction = decode_instruction(CPU.RI, 7).code_op;
                 break;
             case 0b100:
                 ADD((CPU.RI >> 3) && 0x07, CPU.RI & 0x07);
                 next_instru();
-                instruction = decode_instruction(CPU.RI, 7);
+                instruction = decode_instruction(CPU.RI, 7).code_op;
                 break;
             case 0b01101:
                 NOT(CPU.RI & 0x07);
                 next_instru();
-                instruction = decode_instruction(CPU.RI, 7);
+                instruction = decode_instruction(CPU.RI, 7).code_op;
                 break;
             case 0b01100:
                 INC(CPU.RI & 0x07);
                 next_instru();
-                instruction = decode_instruction(CPU.RI, 7);
+                instruction = decode_instruction(CPU.RI, 7).code_op;
                 break;
             case 0b01011:
                 DEC(CPU.RI & 0x07);
                 next_instru();
-                instruction = decode_instruction(CPU.RI, 7);
+                instruction = decode_instruction(CPU.RI, 7).code_op;
                 break;
             case 0b01010:
                 MV(CPU.RI & 0x07);
                 next_instru();
-                instruction = decode_instruction(CPU.RI, 7);
+                instruction = decode_instruction(CPU.RI, 7).code_op;
                 break;
             case 0b01001:
                 LD(CPU.RI & 0x07);
                 next_instru();
-                instruction = decode_instruction(CPU.RI, 7);
+                instruction = decode_instruction(CPU.RI, 7).code_op;
                 break;
             case 0b01000:
                 ST(CPU.RI & 0x07);
                 next_instru();
-                instruction = decode_instruction(CPU.RI, 7);
+                instruction = decode_instruction(CPU.RI, 7).code_op;
                 break;
             case 0b011111:
                 LD2(CPU.RI & 0x07);
                 next_instru();
-                instruction = decode_instruction(CPU.RI, 7);
+                instruction = decode_instruction(CPU.RI, 7).code_op;
                 break;
             case 0b011110:
                 ST2(CPU.RI & 0x07);
                 next_instru();
-                instruction = decode_instruction(CPU.RI, 7);
+                instruction = decode_instruction(CPU.RI, 7).code_op;
                 break;
             case 0b01110011:
                 JMP2(CPU.RI & 0x07);
                 next_instru();
-                instruction = decode_instruction(CPU.RI, 7);
+                instruction = decode_instruction(CPU.RI, 7).code_op;
                 break;
             case 0b01110010:
                 JC();
                 next_instru();
-                instruction = decode_instruction(CPU.RI, 7);
+                instruction = decode_instruction(CPU.RI, 7).code_op;
                 break;
             case 0b01110001:
                 JZ();
                 next_instru();
-                instruction = decode_instruction(CPU.RI, 7);
+                instruction = decode_instruction(CPU.RI, 7).code_op;
                 break;
             case 0b01110000:
                 JMP();
                 next_instru();
-                instruction = decode_instruction(CPU.RI, 7);
+                instruction = decode_instruction(CPU.RI, 7).code_op;
                 break;
         }
     }
     printf("Execution terminée\n");
+}
+
+void
+txt_to_s(const char *filename)
+{
+    char buf[20];
+    int indice;
+    FILE * file = fopen(filename, "r");
+    int file2 = open("test.txt", O_WRONLY | O_TRUNC);
+    if (!file) {
+        perror("Erreur ouverture fichier");
+        fclose(file);
+        exit(EXIT_FAILURE);
+    }
+    char line[256];
+    while (fgets(line, sizeof(line), file)) {
+        unsigned int address;
+        int values[3];
+        int count = sscanf(line, "%x: %x %x %x", &address, &values[0], &values[1], &values[2]);
+        instruction_t inst = decode_instruction(values[0], 7);
+        char * instruction_char = inst.inst_ASM;
+        for (indice = 0; indice < strlen(instruction_char); indice ++) {
+            buf[indice] = instruction_char[indice];
+        }
+        write(file2, buf, indice);
+        write(file2, " ", 1);
+        switch(inst.nb_operande) {
+            case 0:
+                break;
+            case 1:
+                switch(inst.type_op1) {
+                    sprintf(buf, "%d", values[1]); // Convert integer to string
+                    printf("%s\n", buf);
+                    break; 
+                }
+        }
+    }
 }
 
 
@@ -180,4 +216,5 @@ main(int argc, char *argv[]) {
     execute();
     printf("valeur registre 1 : %d\n", CPU.registre.registre[1]);
     printf("valeur registre 2 : %d\n", CPU.registre.registre[2]);
+    txt_to_s(argv[1]);
 }
